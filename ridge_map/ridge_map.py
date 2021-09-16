0ef8e4d04f1cfe817515ded28f7c43965ba9db6c
Alex Miller <alexpmil@gmail.com>
2021-09-15 21:10:52 -0700

    Update ridge_map.py

--- ridge_map/ridge_map.py
+++ ridge_map/ridge_map.py
@@ -29,7 +29,7 @@ class FontManager:
 
     def __init__(
         self,
-        github_url="https://github.com/google/fonts/blob/main/ofl/cinzel/static/Cinzel-Regular.ttf?raw=true",  # pylint: disable=line-too-long
+        github_url="https://github.com/alexmill/ridge_map/blob/master/ridge_map/Cinzel-Regular.ttf?raw=true",  # pylint: disable=line-too-long
     ):
         """
         Lazily download a font.
